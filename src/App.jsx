3b9e867ffd163a6994cb2a62f0fa4c5869bc9e3c
spicyzboss <73651621+spicyzboss@users.noreply.github.com>
2022-07-03 01:32:23 +0700

    feat: summon new mosquito while dead

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { createSignal } from 'solid-js';
-// import Mosquito from './components/Mosquito'
+import { createEffect, createSignal, onMount } from 'solid-js'
+import { createStore } from 'solid-js/store'
 
 const [count, setCount] = createSignal(0);
+const [arr, setArr] = createStore([1, 1, 1, 1])
 
 const App = () => {
   return (
     <div className="flex bg-black">
       <div className="relative w-screen h-screen bg-white max-w-7xl">
-        <Mosquito />
-        <Mosquito />
-        <Mosquito />
-        <Mosquito />
-        <Mosquito />
-        <Mosquito />
+        <For each={arr}>
+          { () => <Mosquito /> }
+        </For>
       </div>
       <p className='p-4 text-xl text-white'>score: { count() }</p>
     </div>
@@ -20,14 +18,14 @@ const App = () => {
 };
 
 function Mosquito() {
-  const [posX, setPosX] = createSignal(0);
-  const [posY, setPosY] = createSignal(0);
+  const [posX, setPosX] = createSignal(Math.floor(Math.random() * 1280) - 32);
+  const [posY, setPosY] = createSignal(Math.floor(Math.random() * 800) + 32);
   const [dead, setDead] = createSignal(false);
 
   const living = setInterval(() => {
     setPosX(Math.floor(Math.random() * 1280) - 32)
     setPosY(Math.floor(Math.random() * 800) + 32)
-  }, Math.floor(500 + Math.random() * 1000))
+  }, Math.floor(500 + Math.random() * 500))
 
   const kill = () => {
     if (!dead()) {
@@ -36,6 +34,7 @@ function Mosquito() {
       ref.classList.remove('cursor-pointer')
       setCount(count() + 1)
       setDead(true)
+      setArr([...arr, 1])
     }
   }
 
@@ -45,7 +44,7 @@ function Mosquito() {
     <img
       ref={ref}
       src="/mosquito.jpeg"
-      className="absolute w-8 h-8 transition-all bg-red-400 cursor-pointer select-none"
+      className="absolute w-16 h-16 transition-all bg-red-400 cursor-pointer select-none"
       onClick={kill}
       style={{
         "top": `${posY()}px`, "left": `${posX()}px`
